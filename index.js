76dd8cfbba415f41049b53363ce532cd10e5f4ed
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-13 16:40:32 +0600

    add product backend is added

--- index.js
+++ index.js
@@ -30,7 +30,15 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
+        });
+
+        //POST
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
         })
+
 
     }
     finally {
@@ -45,4 +53,5 @@ app.get('/', (req, res) => {
 });
 app.listen(port, () => {
     console.log('Listening to port', port);
-})
+});
+
